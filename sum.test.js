f2b46cba54b0ea6bcaefb55e1e123fef318a9d7a
Klaus Reimer <k@ailis.de>
2024-10-29 18:13:16 +0100

    Add user agent output

--- sum.test.js
+++ sum.test.js
@@ -3,6 +3,7 @@ import { sum } from "./sum.js";
 
 describe("sum", () => {
     it("sums numbers", () => {
+        console.log("User Agent:", navigator.userAgent, "\n");
         expect(sum(1, 2)).toBe(3);
     });
 });
